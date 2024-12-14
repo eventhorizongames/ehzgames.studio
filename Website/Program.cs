5fa359e2a720a79b2274d48ceeba62c025ce11e8
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2024-12-14 17:34:29 -0600

    feat: Added Compression Requesting

--- Website/Program.cs
+++ Website/Program.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
